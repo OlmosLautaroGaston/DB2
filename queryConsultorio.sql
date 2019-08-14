f295e0ed06b5e1810ddf34bcb197a629f5c09963
OlmosLautaroGaston <lautaolmoss@gmail.com>
2019-08-14 13:00:01 -0500

    advancedConsultory

--- queryConsultorio.sql
+++ queryConsultorio.sql
@@ -22,6 +22,7 @@ CREATE TABLE odontologo (
 ALTER TABLE agenda
 ADD CONSTRAINT CONSTRAINT_DNI FOREIGN KEY(ID_DNI) REFERENCES odontologo(ID_DNI);
 
+
 /** PACIENTE **/
 
 CREATE TABLE paciente (
@@ -34,28 +35,20 @@ CREATE TABLE paciente (
 /** TIPO DE TURNO **/
 
 CREATE TABLE tipoturno(
-	TRATAMIENTO VARCHAR,
-	PRECIO INTEGER,
-	PRIMARY KEY(TRATAMIENTO,PRECIO)
+	TRATAMIENTO VARCHAR UNIQUE,
+	TIPOTURNO INTEGER,
+	PRIMARY KEY(TRATAMIENTO,TIPOTURNO)
 );
-
-/** RESTRICCIONES TIPO DE TURNO - UNIQUE **/
-
-ALTER TABLE tipoturno
-ADD CONSTRAINT CONSTRA_unique2 
-UNIQUE (PRECIO);
-ALTER TABLE tipoturno
-ADD CONSTRAINT CONSTRA_unique1
-UNIQUE (TRATAMIENTO);
 
 /** TURNO**/
 
 CREATE TABLE turno(
-	TIPO_TURNO VARCHAR,
+	IDTURNO SERIAL UNIQUE,
+	PRECIO INTEGER,
 	TRATAMIENTO VARCHAR REFERENCES tipoturno(TRATAMIENTO),
-	PRECIO INTEGER REFERENCES tipoturno(PRECIO),
 	DNI_ODONT INTEGER REFERENCES odontologo(ID_DNI),
-	DNI_PAC INTEGER REFERENCES paciente(PACIENTE_DNI)
+	DNI_PAC INTEGER REFERENCES paciente(PACIENTE_DNI),
+	PRIMARY KEY(IDTURNO,PRECIO)
 );
 
 /** TURNO DISPONIBLE **/
@@ -66,6 +59,13 @@ CREATE TABLE turnosdisponibles(
 	IDDNI INTEGER REFERENCES odontologo(ID_DNI),
 	ESTADO boolean
 );
+/** SUELDOS**/
+
+CREATE TABLE sueldos(
+	DNI INTEGER REFERENCES odontologo(ID_DNI),
+	MES VARCHAR,
+	TURNOS INTEGER REFERENCES turno(IDTURNO)
+);
 
 /** DATOS **/
 
@@ -74,15 +74,15 @@ SELECT * FROM agenda;
 SELECT * FROM paciente;
 SELECT * FROM turno;
 SELECT * FROM tipoturno;
-SELECT * FROM turnosdisponibles;
+SELECT * FROM sueldos;
 
 /** DATOS ODONTOLOGOS **/
 
-INSERT INTO odontologo VALUES (33421123,'Jorge','Videla');
-INSERT INTO odontologo VALUES (41155173,'Lautaro','Martin');
-INSERT INTO odontologo VALUES (12333124,'Rafael','Tisera');
-INSERT INTO odontologo VALUES (43212312,'Carlos','Acosta');
-INSERT INTO odontologo VALUES (56787767,'Mariel','Ojeda');
+INSERT INTO odontologo VALUES (33421123,'Jorge','Videla','1998-05-21');
+INSERT INTO odontologo VALUES (41155173,'Lautaro','Martin','2005-10-22');
+INSERT INTO odontologo VALUES (12333124,'Rafael','Tisera','2001-02-13');
+INSERT INTO odontologo VALUES (43212312,'Carlos','Acosta','1998-01-21');
+INSERT INTO odontologo VALUES (56787767,'Mariel','Ojeda','2018-12-14');
 
 /** DATOS AGENDA **/
 
@@ -113,18 +113,51 @@ INSERT INTO paciente VALUES(26548495,'Martina','Martinez','1968-02-22');
 INSERT INTO paciente VALUES(32159489,'Julia','Rodriguez','1999-12-04');
 /** DATOS TURNO **/
 
+INSERT INTO turno VALUES(1,250,'conducto',41155173,39852658);
+INSERT INTO turno VALUES(2,400,'Endodoncia',12333124,25698741);
+INSERT INTO turno VALUES(3,300,'Ortodoncia',12333124,65481898);
+INSERT INTO turno VALUES(4,100,'Periodoncia',41155173,26548495);
+INSERT INTO turno VALUES(5,150,'Limpieza',56787767,32159489);
+INSERT INTO turno VALUES(6,250,'conducto',41155173,39852658);
+INSERT INTO turno VALUES(7,400,'Endodoncia',56787767,25698741);
+INSERT INTO turno VALUES(8,300,'Ortodoncia',12333124,65481898);
+INSERT INTO turno VALUES(9,100,'Periodoncia',41155173,26548495);
+INSERT INTO turno VALUES(10,150,'Limpieza',56787767,32159489);
+INSERT INTO turno VALUES(11,150,'Limpieza',33421123,32159489);
 
 /** DATOS TIPO DE TURNO **/
 
-INSERT INTO tipoturno VALUES('conducto',250);
-INSERT INTO tipoturno VALUES('Endodoncia',400);
-INSERT INTO tipoturno VALUES('Ortodoncia',300);
-INSERT INTO tipoturno VALUES('Periodoncia',100);
-INSERT INTO tipoturno VALUES('Limpieza',150);
-INSERT INTO tipoturno VALUES('Restauracion ',375);
+INSERT INTO tipoturno VALUES('conducto',1);
+INSERT INTO tipoturno VALUES('Endodoncia',2);
+INSERT INTO tipoturno VALUES('Ortodoncia',2);
+INSERT INTO tipoturno VALUES('Periodoncia',1);
+INSERT INTO tipoturno VALUES('Limpieza',1);
+INSERT INTO tipoturno VALUES('Restauracion ',2);
 
-INSERT INTO turno VALUES('Simple','conducto',250,41155173,39852658);
-INSERT INTO turno VALUES('Doble','Endodoncia',400,12333124,25698741);
-INSERT INTO turno VALUES('Doble','Ortodoncia',300,12333124,65481898);
-INSERT INTO turno VALUES('Simple','Periodoncia',100,41155173,26548495);
-INSERT INTO turno VALUES('Simple','Limpieza',150,56787767,32159489);
+/** DATOS SUELDOS **/
+
+INSERT INTO sueldos VALUES(41155173,'Enero',1);
+INSERT INTO sueldos VALUES(41155173,'Enero',4);
+INSERT INTO sueldos VALUES(41155173,'Enero',6);
+INSERT INTO sueldos VALUES(56787767,'Enero',5);
+INSERT INTO sueldos VALUES(56787767,'Enero',7);
+INSERT INTO sueldos VALUES(56787767,'Enero',10);
+INSERT INTO sueldos VALUES(12333124,'Enero',2);
+INSERT INTO sueldos VALUES(12333124,'Enero',3);
+INSERT INTO sueldos VALUES(12333124,'Enero',8);
+INSERT INTO sueldos VALUES(33421123,'Enero',11);
+
+select * from odontologo
+update odontologo cascade set id_dni=40155173 where nombre='Lautaro';
+update turno cascade set dni_odont=40155173 where dni_odont=41155173;
+
+UPDATE odontologo t1
+SET t1.id_dni = 40155173
+FROM turno t2
+WHERE t1.id_dni = t2.dni_odont and t1.id_dni=41155173;
+
+UPDATE odontologo AS t1
+SET id_dni = 40155173
+FROM turno AS t2
+ON t1.id_dni = t2.dni_odont
+WHERE t1.id_dni=4155173; 
